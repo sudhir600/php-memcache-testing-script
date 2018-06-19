30eaf5cb29f78b3ed06bcb1aeb2ba4ed9c0ca234
SUDHIR K GUPTA <sudhirgupta.456@gmail.com>
2018-06-19 16:59:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$memcache = memcache_connect('localhost', 11211);
+
+if ($memcache) {
+	$memcache->set("str_key", "Hello memcached");
+	$memcache->set("num_key", 123);
+
+	$object = new StdClass;
+	$object->attribute = 'test';
+	$memcache->set("obj_key", $object);
+
+	$array = Array('assoc'=>123, 456, 789);
+	$memcache->set("arr_key", $array);
+
+	var_dump($memcache->get('str_key'));
+	var_dump($memcache->get('num_key'));
+	var_dump($memcache->get('obj_key'));
+}
+else {
+	echo "Memcached Connection failed";
+}
+	
+?>
